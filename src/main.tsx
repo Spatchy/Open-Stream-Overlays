001353488d773c96b96f8f60773cb64c8ee098d6
Spatchy <spatchy@spatchy.net>
2023-12-30 00:54:46 +0000

    init react-router and error page

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
 import App from "./App.tsx"
 import "./index.css"
+import ErrorPage from "./components/ErrorPage/ErrorPage.tsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  }
+])
+
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
